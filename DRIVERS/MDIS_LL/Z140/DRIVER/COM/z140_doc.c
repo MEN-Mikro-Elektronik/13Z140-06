7bdce38210fc2cb4112b514acc90f3b81e96914e
Dieter Pfeuffer <dieter.pfeuffer@men.de>
2016-09-20 08:37:43 +0200

    documentation added

---
+++ DRIVERS/MDIS_LL/Z140/DRIVER/COM/z140_doc.c
@@ -0,0 +1,87 @@
+/*********************  P r o g r a m  -  M o d u l e ***********************/
+/*!
+*        \file  z140_doc.c
+*
+*      \author  dieter.pfeuffer@men.de
+*        $Date: $
+*    $Revision: $
+*
+*      \brief   User documentation for Z140 device driver
+*
+*     Required: -
+*
+*     \switches -
+*/
+/*-------------------------------[ History ]--------------------------------
+*
+* $Log: z140_doc.c,v $
+*---------------------------------------------------------------------------
+* (c) Copyright 2016 by MEN Mikro Elektronik GmbH, Nuernberg, Germany
+****************************************************************************/
+
+/*! \mainpage
+    This is the documentation of the MDIS5 low-level driver for the MEN 16Z140
+	Frequency Counter IP core.
+
+	\n \section FuncDesc Functional Description
+
+	\n \subsection IpCore IP Core
+
+    The 16Z140 is a Frequency Counter IP core that performs different analysis
+	on either a single signal input or on a quadrature encoded input with two signals.
+
+	The Frequency Counter IP core supports the following functions:
+	- Frequency Measurement of Signal A
+	- Frequency Measurement of Signal B
+	- Direction Detection
+	- Standstill Detection
+	- Rolling Detection
+	- Distance Measurement
+	- Built-In Test Frequency Generator
+
+	\n \subsection Driver Driver
+
+    The driver supports controlling of the Frequency Counter via M_setstat() and M_getstat(),
+	see \ref getstat_setstat_codes "Getstat/Setstat codes".
+
+    When the first path is opened to an 16Z140 device, the IP core and the driver are being
+    initialized.
+    \n
+
+    \n \section api_functions Supported API Functions
+
+    <table border="0">
+    <tr>
+    <td><b>API Function</b></td>
+    <td><b>Functionality</b></td>
+    <td><b>Corresponding Low - Level Function</b></td></tr>
+
+    <table border="0">
+    <tr><td><b>API Function</b></td> <td><b>Functionality</b></td>   <td><b>Corresponding Low - Level Function</b></td></tr>
+    <tr><td>M_open()</td>            <td>Open device</td>            <td>Z140_Init()</td></tr>
+    <tr><td>M_close()</td>           <td>Close device</td>           <td>Z140_Exit()</td></tr>
+    <tr><td>M_setstat()</td>         <td>Set device parameter</td>   <td>Z140_SetStat()</td></tr>
+    <tr><td>M_getstat()</td>         <td>Get device parameter</td>   <td>Z140_GetStat()</td></tr>
+    <tr><td>M_errstringTs()</td>     <td>Generate error message</td> <td>-</td></tr>
+	</table>
+
+
+	\n \section codes Z140 specific Getstat/Setstat codes
+	see \ref getstat_setstat_codes "Getstat/Setstat codes"
+
+
+    \n \section programs Overview of provided programs
+
+    \subsection z140_simp Simple example program for Frequency Counter driver
+    z140_simp.c (see example section)
+
+    \subsection z140_ctrl Control tool for Frequency Counter driver
+    z140_ctrl.c (see example section)
+*/
+
+/** \example z140_simp.c */
+/** \example z140_ctrl.c */
+
+/*! \page dummy
+\menimages
+*/
