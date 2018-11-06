db20aa752c617b519d154367aae51ea2fa5a9dc9
KonradJPE <ktomasik@jpembedded.eu>
2018-11-06 14:05:55 +0100

    Add GPL license to file

--- DRIVERS/MDIS_LL/Z140/DRIVER/COM/z140_doc.c
+++ DRIVERS/MDIS_LL/Z140/DRIVER/COM/z140_doc.c
@@ -11,16 +11,24 @@
 *     Required: -
 *
 *     \switches -
-*/
-/*-------------------------------[ History ]--------------------------------
-*
-* $Log: z140_doc.c,v $
-* Revision 1.1  2017/03/30 13:13:41  DPfeuffer
-* Initial Revision
 *
 *---------------------------------------------------------------------------
 * (c) Copyright 2016 by MEN Mikro Elektronik GmbH, Nuernberg, Germany
 ****************************************************************************/
+/*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 /*! \mainpage
     This is the documentation of the MDIS5 low-level driver for the MEN 16Z140
